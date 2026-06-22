330c19faeff18c9eb5dc584aa98a033d7f7872b8
siliconvalleyar-oss <siliconvalleyar@gmail.com>
2026-06-22 11:42:48 -0300

    fix: add missing #include <iostream> in MenuScene.cpp

--- src/engine/MenuScene.cpp
+++ src/engine/MenuScene.cpp
@@ -2,6 +2,7 @@
 #include <engine/GameEngine.hpp>
 #include <cstring>
 #include <cstdio>
+#include <iostream>
 
 static uint16_t rgb(uint8_t r, uint8_t g, uint8_t b) { return RGB565CONVERT(r, g, b); }
 
