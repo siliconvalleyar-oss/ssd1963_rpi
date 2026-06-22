2504641ae8f904b6e3b95b40b0f8807fe441d7bb
siliconvalleyar-oss <siliconvalleyar@gmail.com>
2026-06-22 14:02:13 -0300

    rotate bitmap font 90 degrees CW

--- compile_remote.sh
+++ compile_remote.sh
@@ -3,12 +3,15 @@
 set -e
 
 PI_USER="pi"
-PI_HOST="raspi.local"
+PI_HOST="192.168.1.37"
 PI_PASS="zero"
 PI_DIR="/home/pi/src/ssd1963_photo_png_2026_sucess_rgb"
 
 echo "=== Compilando en ${PI_USER}@${PI_HOST}:${PI_DIR} ==="
-sshpass -p "${PI_PASS}" ssh "${PI_USER}@${PI_HOST}" "
+# Agregar host key si no existe
+ssh-keyscan -H "${PI_HOST}" >> ~/.ssh/known_hosts 2>/dev/null
+
+sshpass -p "${PI_PASS}" ssh -o StrictHostKeyChecking=no "${PI_USER}@${PI_HOST}" "
     cd ${PI_DIR} && \
     git pull origin feature/qr-menu-docs && \
     make clean && \
