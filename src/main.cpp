680db1169b2e92310d9d558ef100265fc1ecdd80
siliconvalleyar-oss <siliconvalleyar@gmail.com>
2026-06-25 15:37:32 -0300

    fix: corregir errores de compilación en test directo

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,11 @@
 #include <engine/PatternScene.hpp>
 #include <engine/SpriteViewerScene.hpp>
 #include <engine/sprite_list.hpp>
+#include <unistd.h>
+
+static uint16_t rgb565(uint8_t r, uint8_t g, uint8_t b) {
+    return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
+}
 
 int main() {
     GameEngine engine;
@@ -17,7 +22,7 @@ int main() {
     FontDemoScene     font_demo(engine.display());
     SpaceShooterScene shooter(engine.display());
     ImageViewerScene  viewer(engine.display(), "assets/capibaras.rgb565", "CAPIBARAS");
-    ColorBlocksScene  colors(engine.display());
+    ColorBlocksScene  color_blocks(engine.display());
     PatternScene      patterns(engine.display());
     SpriteViewerScene sprites(engine.display());
 
@@ -27,27 +32,23 @@ int main() {
     menu.add_item("SPACE SHOOTER",  &shooter);
     menu.add_item("VER FOTO",       &viewer);
     menu.add_item("NAVES",          &sprites);
-    menu.add_item("COLORES",        &colors);
+    menu.add_item("COLORES",        &color_blocks);
     menu.add_item("PATRONES",       &patterns);
     menu.add_item("SALIR",          nullptr);
 
     // === TEST: renderizado directo (sin FrameBuffer) ===
     SSD1963& d = engine.display();
-    static constexpr uint16_t RGB(uint8_t r, uint8_t g, uint8_t b) {
-        return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
-    }
-    uint16_t colors[] = { RGB(255,0,0), RGB(0,255,0), RGB(0,0,255),
-                          RGB(255,255,0), RGB(255,0,255), RGB(0,255,255),
-                          RGB(255,255,255) };
+    uint16_t cols[] = { rgb565(255,0,0), rgb565(0,255,0), rgb565(0,0,255),
+                        rgb565(255,255,0), rgb565(255,0,255), rgb565(0,255,255),
+                        rgb565(255,255,255) };
     for (uint32_t flash = 0; flash < 7; flash++) {
-        d.clear_screen(colors[flash % 7]);
-        d.draw_rect(10, 10, 460, 252, RGB(255,255,0));
-        d.draw_string(20, 20, "TEST DIRECTO", RGB(255,255,255), colors[flash % 7]);
-        d.draw_string(20, 30, "SIN FRAMEBUFFER", RGB(200,200,200), colors[flash % 7]);
-        engine.engine_delay(300);
+        d.clear_screen(cols[flash]);
+        d.draw_rect(10, 10, 460, 252, rgb565(255,255,0));
+        d.draw_string(20, 20, "TEST DIRECTO", rgb565(255,255,255), cols[flash]);
+        d.draw_string(20, 30, "SIN FRAMEBUFFER", rgb565(200,200,200), cols[flash]);
+        usleep(300000);
     }
-    d.clear_screen(RGB(8,8,28));
-
+    d.clear_screen(rgb565(8,8,28));
     // === FIN TEST ===
 
     engine.set_menu_scene(&menu);
