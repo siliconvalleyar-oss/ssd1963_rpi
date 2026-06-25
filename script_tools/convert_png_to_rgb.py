c63c0110197bb5ded51072e36ab97ca4ad5a9f88
siliconvalleyar-oss <siliconvalleyar@gmail.com>
2026-06-25 13:29:19 -0300

    fix: convert_png_to_rgb.py maneja todos los modos PNG con convert('RGBA') unificado

--- script_tools/convert_png_to_rgb.py
+++ script_tools/convert_png_to_rgb.py
@@ -24,28 +24,16 @@ def rgba_to_rgb565(r, g, b, a=255):
     return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
 
 def convert_to_rgb565(filename, output):
-    img = Image.open(filename)
-    if img.mode == 'RGBA':
-        px = img.load()
-        w, h = img.size
-        with open(output, 'wb') as f:
-            f.write(w.to_bytes(2, 'big'))
-            f.write(h.to_bytes(2, 'big'))
-            for y in range(h):
-                for x in range(w):
-                    r, g, b, a = px[x, y]
-                    f.write(rgba_to_rgb565(r, g, b, a).to_bytes(2, 'big'))
-    else:
-        img_rgb = img.convert('RGB')
-        w, h = img_rgb.size
-        with open(output, 'wb') as f:
-            f.write(w.to_bytes(2, 'big'))
-            f.write(h.to_bytes(2, 'big'))
-            for y in range(h):
-                for x in range(w):
-                    r, g, b = img_rgb.getpixel((x, y))
-                    rgb565 = ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
-                    f.write(rgb565.to_bytes(2, 'big'))
+    img = Image.open(filename).convert('RGBA')
+    px = img.load()
+    w, h = img.size
+    with open(output, 'wb') as f:
+        f.write(w.to_bytes(2, 'big'))
+        f.write(h.to_bytes(2, 'big'))
+        for y in range(h):
+            for x in range(w):
+                r, g, b, a = px[x, y]
+                f.write(rgba_to_rgb565(r, g, b, a).to_bytes(2, 'big'))
     print(f"Converted {filename} -> {output} ({w}x{h})")
 
 if __name__ == "__main__":
